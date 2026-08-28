645cd34c92fcf9e17ccce9cfdca2483b2cc892ef
Radouane <radjam.r@gmail.com>
2026-08-28 07:26:49 +0200

    Signin OK

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,7 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/Galerie", " La Galerie", "/pages/Galerie.html"),
+    new Route("/Signin", " Connexion", "/pages/Signin.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
