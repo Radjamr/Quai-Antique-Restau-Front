cab922d0d75223239bfb972c458e5da6253d9b46
Radouane <radjam.r@gmail.com>
2026-08-28 17:45:37 +0200

    Reservation en cours

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -8,7 +8,9 @@ export const allRoutes = [
     new Route("/Signup", "Inscription", "/pages/auth/Signup.html"),
     new Route("/Account", "Mon Compte", "/pages/auth/Account.html"),
     new Route("/EditPassword", "Modifier mon Mot de Passe", "/pages/auth/EditPassword.html"),
+    new Route("/AllResa", "Vos Réservation", "/pages/reservation/AllResa.html"),
 ];
+
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
