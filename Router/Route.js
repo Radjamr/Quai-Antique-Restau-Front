2d350966182d82a57feb01795b8c5fa85a92f19e
Radouane <radjam.r@gmail.com>
2026-08-29 15:56:57 +0200

    Autorisation des pages en fonction des roles

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,17 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
+/*
+[]--> Tout le monde peut y acceder
+["Disconnected"] --> Réserver aux utilisateur déconnecté
+["Client"] --> Réserver aux utilisateur avec le rôle Client
+["admin"] --> Réserver aux utilisateur avec le rôle Admin
+["admin", "client"] --> Réserver aux utilisateur avec le rôle Client ou admin
+*/
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Galerie", "La Galerie", "/pages/Galerie.html"),
-    new Route("/Signin", "Connexion", "/pages/auth/Signin.html", "/js/auth/signin.js"),
-    new Route("/Signup", "Inscription", "/pages/auth/Signup.html", "/js/auth/signup.js"),
-    new Route("/Account", "Mon Compte", "/pages/auth/Account.html"),
-    new Route("/EditPassword", "Modifier mon Mot de Passe", "/pages/auth/EditPassword.html"),
-    new Route("/AllResa", "Vos Réservation", "/pages/reservation/AllResa.html"),
-    new Route("/reserver", "Réserver", "/pages/reservation/reserver.html"),
+    new Route("/", "Accueil", "/pages/home.html",[]),
+    new Route("/Galerie", "La Galerie", "/pages/Galerie.html",[]),
+    new Route("/Signin", "Connexion", "/pages/auth/Signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/Signup", "Inscription", "/pages/auth/Signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/Account", "Mon Compte", "/pages/auth/Account.html", ["client", "admin"]),
+    new Route("/EditPassword", "Modifier mon Mot de Passe",["client", "admin"] , "/pages/auth/EditPassword.html"),
+    new Route("/AllResa", "Vos Réservation", "/pages/reservation/AllResa.html",["client"]),
+    new Route("/reserver", "Réserver", "/pages/reservation/reserver.html",["client"]),
 
 
 
