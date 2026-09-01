ae1b5c9963a12bd8133bbe309c6c10dc5656c04f
Radouane <radjam.r@gmail.com>
2026-09-01 17:44:41 +0200

    Connexion (apitoken valide ect)+ redirection vers la pages d'accueil

--- js/auth/signin.js
+++ js/auth/signin.js
@@ -1,21 +1,46 @@
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
+const signinForm = document.getElementById("signinForm");
 
 btnSignin.addEventListener("click", checkCredentials);
 
 function checkCredentials(){
+    let dataForm = new FormData(signinForm);
    //ici, il faudra appeler API pour vérifier les credentials en BDD
-    if(mailInput.value == "test@mail.com" && passwordInput.value == "123"){
-        //Il faudra récuper le vrai Token        
-        const token = "oskfnzokfklfnpzdzpifnzmpfmpznfpozfjpznfpzfnpzjfpzfnpz";
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({
+        "username": dataForm.get("email"),
+        "password": dataForm.get("mdp")
+    });
+
+    let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch(apiUrl+"login", requestOptions)
+    .then(response => {
+        if(response.ok){
+            return response.json();
+        }
+        else{
+            mailInput.classList.add("is-invalid");
+            passwordInput.classList.add("is-invalid");
+        }
+    })
+    .then(result => {
+        //Il faudra récuper le vrai Token    fait    
+        const token = result.apiToken;
         setToken(token);
-        setCookie(RoleCookieName, "admin", 7),
+        //placer ce token en cookie fait
+        setCookie(RoleCookieName, result.roles[0], 7),
         window.location.replace("/");
-    }
-    else{
-        mailInput.classList.add("is-invalid");
-        passwordInput.classList.add("is-invalid");
-    }
+    })
+    .catch(error => console.log('error', error));
 }
 
